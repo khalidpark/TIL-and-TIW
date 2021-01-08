d9a1e3e98830a064257da38ba1aa2af94c72944b
khalidpark2029 <70706704+khalidpark@users.noreply.github.com>
2021-01-08 23:48:00 +0900

    Add files via upload

---
+++ NomadCoders/scrapper_210108.py
@@ -0,0 +1,36 @@
+a_string = "text"
+a_number = 3
+a_float = 3.12
+a_boolean = True
+a_none = None
+
+#python은 2가지 sequence type이 있다
+#1) list , 2)tuple
+
+#list = common , mutable
+#tuple = common , not mutable
+days = ["mon", "tue", "wed"] , types = list
+days = ("mon", "tue", "wed") , types = tuple
+
+name = "nico"
+age = 29
+fav_food = ["kimchi" , "ssasimi"]
+이것들은 변수들
+
+#딕셔너리
+nico = {
+    "name" : "nico",
+    "age" : 29,
+    "fav_food" : ["kimchi" , "ssasimi"]
+}
+
+print(nico)
+print(nico["name"])
+
+print(nico)
+nice["handsome"] = True
+print(nico)
+
+리스트 []
+튜플 ()
+딕셔너리 {}
